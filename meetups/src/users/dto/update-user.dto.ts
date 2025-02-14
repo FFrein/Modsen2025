b23bdba96c4bdde502fbe2db7bcde7f725b0bd9a
FFrein <pesetsky2003g@gmail.com>
2025-02-14 22:14:18 +0300

    feat: user entity and add swagger api property to dto

--- meetups/src/users/dto/update-user.dto.ts
+++ meetups/src/users/dto/update-user.dto.ts
@@ -1,13 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({ description: 'ID пользователя', example: 1 })
   @IsNumber()
   @IsNotEmpty()
   id: number;
 
-  @IsNumber()
+  @ApiProperty({
+    enum: UserRole,
+    required: false,
+    description: 'Роль пользователя',
+  })
+  @IsEnum(UserRole)
   @IsOptional()
-  role: string;
+  role: UserRole;
 }
--- meetups/src/users/entities/user.entity.ts
+++ meetups/src/users/entities/user.entity.ts
@@ -1,1 +1,10 @@
-export class User {}
+export enum UserRole {
+  USER = 'USER',
+  ORGANIZER = 'ORGANIZER',
+}
+export class User {
+  id: number;
+  role: UserRole;
+  username: string;
+  password: string;
+}
