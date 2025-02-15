c997a4e7c09ea174172be81f2affb242cb32a50a
FFrein <pesetsky2003g@gmail.com>
2025-02-15 13:55:39 +0300

    feat: написал авторизацию по инструкции nest passport
    Реаоизовал local strategy и jwt strategy

--- meetups/src/app.module.ts
+++ meetups/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MeetupModule } from './meetup/meetup.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, MeetupModule],
+  imports: [UsersModule, MeetupModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ meetups/src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'Засунуть потом токен в env',
+};
