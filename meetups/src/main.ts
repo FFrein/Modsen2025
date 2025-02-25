450f88bed61c0d533db85a5bfb63e3307761ea50
FFrein <pesetsky2003g@gmail.com>
2025-02-25 11:48:33 +0300

    feat: exception filter

--- meetups/src/main.ts
+++ meetups/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { GlobalExceptionFilter } from './http-exception.filter';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
